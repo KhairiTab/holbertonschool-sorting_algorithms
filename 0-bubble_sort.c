6d352b36a69dce520c42f63e074fe1c2673e7346
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-09-26 13:38:32 +0100

    Fixing

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,46 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * swap - Swap values of start and next index.
+ * @array: the array with start and next values.
+ * @start: start point of the array.
+ * @next: next point of the array.
+ * Return: nothing.
+ */
+void swap(int *array, size_t start, size_t next)
+{
+	size_t temp = array[next];
+
+	array[next] = array[start];
+	array[start] = temp;
+}
+
+/**
+ * bubble_sort - Sort an array with bubble algorithm.
+ * @array: the array to sort.
+ * @size: size of the array.
+ * Return: nothing
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t start = 0;
+	size_t next = 0;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	if (!array || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
-			}
-		}
-}
 
+	while (start < size)
+	{
+		next = 0;
+		while (next < size - 1)
+		{
+			if (array[next] > array[next + 1])
+			{
+				swap(array, next, next + 1);
+				print_array(array, size);
+			}
+			next++;
+		}
+		start++;
+	}
+}
